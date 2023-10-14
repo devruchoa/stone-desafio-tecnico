ed52afee4deef42e65447f60f53e5ad9bad7e605
devruchoa <ronaldo.uchoa.pessoal@gmail.com>
2023-10-14 13:40:01 -0300

    feat: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+import { ResultComponent } from './components/result/result.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'form', pathMatch: 'full' },
+  { path: 'form', component: FormComponent },
+  { path: 'result', component: ResultComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
